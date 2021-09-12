3a85b7dc66a481b63ec93a3f6b2bbe55629f03cc
Esther Ibáñez González <eibanez@ces.vocento.com>
2021-09-12 19:33:12 +0200

    Added main execution

--- main.go
+++ main.go
@@ -5,18 +5,22 @@ import (
 	"musement/config"
 	"musement/musement"
 	"musement/utils"
+	"musement/weather"
 	"net/http"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
-
 	config.LoadConfig()
-
 	client := utils.NewApiClient(http.Client{})
-
 	musementAPIService := musement.NewMusementApiService(client)
+	weatherAPIService := weather.NewWeatherApiService(client)
 
 	cities, _ := musementAPIService.GetCities()
-	fmt.Println(cities)
+	days := 2
+	for _, city := range cities.Cities {
+		w, _ := weatherAPIService.GetWeather(city.Latitude, city.Longitude, days)
+		str := fmt.Sprintf("Processed city %s | %s - %s", city.Name, w.Forecasts.ForecastsDay[0].Day.Condition.Text, w.Forecasts.ForecastsDay[1].Day.Condition.Text)
+		fmt.Println(str)
+		//break
+	}
 }
